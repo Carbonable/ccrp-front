19c1b8a9e30fcc2c2fe32f4558b878629e84d054
François Schuers <francois.schuers@gmail.com>
2024-07-24 09:39:50 +0200

    feat: logout

--- src/components/common/Logout.tsx
+++ src/components/common/Logout.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useAuth } from "@/context/AuthContext";
+import { PowerIcon } from "@heroicons/react/24/outline";
 import { useRouter } from "next/navigation";
 
 export default function LogoutButton() {
@@ -11,6 +12,10 @@ export default function LogoutButton() {
     router.push('/login');
   };
   return (
-    <button onClick={handleLogout}>Logout</button>
+    <div onClick={handleLogout} className="flex items-center justify-start font-bold">
+      <PowerIcon className="w-4 h-4 mr-2" />
+      <button className="uppercase">Logout</button>
+    </div>
+    
   )
 }
