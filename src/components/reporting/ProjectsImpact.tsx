1d8042f3b321a52e3d99b78a904c7be62413a682
François Schuers <francois.schuers@gmail.com>
2024-08-22 10:41:33 +0200

    feat: migrate reporting

---
+++ src/components/reporting/ProjectsImpact.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { GET_IMPACT_METRICS } from "@/graphql/queries/impact";
+import { CARBONABLE_COMPANY_ID, GLOBAL_IMPACT_LINK } from "@/utils/constant";
+import { useQuery } from "@apollo/client";
+import ImpactComponent from "../common/impact/ImpactComponent";
+
+export default function ProjectsImpact() {
+    const { loading, error, data, refetch } = useQuery(GET_IMPACT_METRICS, {
+        variables: {
+            view: {
+                company_id: CARBONABLE_COMPANY_ID
+            }
+        }
+    });
+   
+    return (
+        <ImpactComponent
+            loading={loading}
+            error={error}
+            data={data}
+            refetch={refetch}
+            link={GLOBAL_IMPACT_LINK}
+        />
+    )
+}
