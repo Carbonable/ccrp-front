c8ecf39d10bfb12121e5b4c1a77b93df557b4afe
LouGel <l.gelinet@protonmail.ch>
2024-09-05 09:46:37 +0200

    👷: round if float for displays

---
+++ src/utils/numbers.ts
@@ -0,0 +1,15 @@
+type Maybe<T> = T | null;
+
+export function roundIfFloat(
+  value: Maybe<number> | number | undefined,
+  decimals: number = 2
+): string {
+  if (value === null || value === undefined) {
+    return "n/a";
+  }
+
+  if (Number.isInteger(value)) {
+    return value.toString();
+  }
+  return value.toFixed(decimals).replace(/\.?0+$/, "");
+}
