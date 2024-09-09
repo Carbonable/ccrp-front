bd27b521f20fad2f72736456a13c31edf536a546
LouGel <l.gelinet@protonmail.ch>
2024-09-09 16:28:38 +0200

    :broom::app duplicate

--- app/dashboard/reporting/page.tsx
+++
@@ -1,9 +0,0 @@
-import ProjectsImpact from "@/components/reporting/ProjectsImpact";
-
-export default function ReportingPage() {
-    return (
-      <div>
-        <ProjectsImpact />
-      </div>
-    )
-  }
