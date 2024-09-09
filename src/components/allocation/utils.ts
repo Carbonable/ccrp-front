ee756e7a4ab07278cc8ef3444f8d823efbf29d5c
LouGel <l.gelinet@protonmail.ch>
2024-09-09 17:01:22 +0200

    👷: added onKeyDown, handle amountPerc instead of amounts

---
+++ src/components/allocation/utils.ts
@@ -0,0 +1,11 @@
+export function onlyPositiveInteger(e: React.KeyboardEvent<HTMLInputElement>) {
+  // Prevent entering non-numeric characters, "e", ".", and "-"
+  if (
+    e.key === "e" ||
+    e.key === "." ||
+    e.key === "-" ||
+    (!/[0-9]/.test(e.key) && e.key !== "Backspace" && e.key !== "Delete")
+  ) {
+    e.preventDefault();
+  }
+}
