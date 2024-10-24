a7ff994ec3b22173eb779620a9dfcac0d10b9c4d
François Schuers <francois.schuers@gmail.com>
2024-10-24 15:47:14 +0200

    change select component

--- src/components/allocation/ProjectsList.tsx
+++ src/components/allocation/ProjectsList.tsx
@@ -2,15 +2,8 @@
 import { Project } from '@/graphql/__generated__/graphql';
 import { GET_PROJECTS } from '@/graphql/queries/projects';
 import { useQuery } from '@apollo/client';
-import {
-  Listbox,
-  ListboxButton,
-  ListboxOption,
-  ListboxOptions,
-  Transition,
-} from '@headlessui/react';
-import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/24/outline';
-import { Fragment, useEffect } from 'react';
+import { Select, SelectItem } from '@nextui-org/react';
+import { useEffect, useState } from 'react';
 
 export default function ProjectsList({
   selectedProject,
@@ -20,13 +13,25 @@ export default function ProjectsList({
   setSelectedProject: (project: Project) => void;
 }) {
   const { loading, error, data } = useQuery(GET_PROJECTS);
+  const [value, setValue] = useState<string>('');
+
+  const handleSelectionChange = (e: any) => {
+    if (!e.target.value || projects === undefined || projects.length === 0) {
+      return;
+    }
+
+    setValue(e.target.value);
+    const selectProject = projects.find((project) => project.id === e.target.value);
+    if (selectProject) {
+      setSelectedProject(selectProject);
+    }
+  };
 
   if (error) {
     console.error(error);
   }
 
   const projects: Project[] = data?.projects;
-
   useEffect(() => {
     if (projects && projects.length > 0) {
       setSelectedProject(projects[0]);
@@ -46,47 +51,17 @@ export default function ProjectsList({
   return (
     <>
       <div className="mb-2 text-left font-light uppercase text-neutral-200">Select Project</div>
-      <Listbox value={selectedProject} onChange={setSelectedProject}>
-        <ListboxButton className="relative w-full cursor-pointer rounded-lg bg-opacityLight-5 py-3 pl-3 pr-10 text-left shadow-md focus:outline-none sm:text-sm">
-          <span className="block truncate">{selectedProject.name}</span>
-          <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-            <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-          </span>
-        </ListboxButton>
-        <Transition
-          as={Fragment}
-          leave="transition ease-in duration-100"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <ListboxOptions className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-neutral-600 py-2 text-base shadow-lg ring-1 ring-neutral-700 focus:outline-none sm:text-sm">
-            {projects.map((project: Project) => (
-              <ListboxOption
-                key={project.id}
-                className={({ focus }) =>
-                  `relative cursor-pointer select-none py-2 pl-10 pr-4 text-left ${
-                    focus ? 'bg-neutral-500 text-neutral-300' : 'text-neutral-300'
-                  }`
-                }
-                value={project}
-              >
-                {({ selected }) => (
-                  <>
-                    <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>
-                      {project.name}
-                    </span>
-                    {selected ? (
-                      <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-neutral-300">
-                        <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                      </span>
-                    ) : null}
-                  </>
-                )}
-              </ListboxOption>
-            ))}
-          </ListboxOptions>
-        </Transition>
-      </Listbox>
+      <Select
+        variant="flat"
+        className="w-full select-component"
+        classNames={{popoverContent: 'bg-neutral-800', value: '!text-neutral-200'}}
+        selectedKeys={[value]}
+        onChange={handleSelectionChange}
+      >
+        {projects.map((project) => (
+          <SelectItem key={project.id}>{project.name}</SelectItem>
+        ))}
+      </Select>
     </>
   );
 }
