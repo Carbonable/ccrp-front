05c4e1544ad51773ed5dfd59dcc4b909a8cfc149
LouGel <l.gelinet@protonmail.ch>
2024-11-13 15:43:54 +0100

    debug: added dev reset db mode

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,13 +1,18 @@
+
 import UploadCSV from '@/components/admin/UploadCSV';
+import DangerButton from '@/components/dev/BigRedButton';
 import { supportedFileTypes } from '@/types/admin';
 
 export default function AdminPage() {
+  
+  let env = process.env.NODE_ENV;
   return (
     <div className="container mx-auto p-4">
       <h1 className="mb-4 text-2xl font-bold">Data upload</h1>
       {supportedFileTypes.map((type) => (
         <UploadCSV key={type} type={type} />
       ))}
+      {(env === 'test' || env ==='development') &&<DangerButton />}
     </div>
   );
 }
