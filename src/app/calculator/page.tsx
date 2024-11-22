037339eb65afb8d7d981833e1efe9acf66ac1132
François Schuers <francois.schuers@gmail.com>
2024-11-22 11:45:36 +0100

    feat: hide scrollbar

--- src/app/calculator/page.tsx
+++ src/app/calculator/page.tsx
@@ -2,13 +2,18 @@ export default function CalculatorPage() {
   const iframeUrl = 'https://kalculator.fly.dev';
 
   return (
-    <div>
+    <div className="w-full">
       <iframe
         key={iframeUrl}
         src={iframeUrl}
         title="Carculator"
         sandbox="allow-scripts allow-same-origin"
-        style={{ width: '100%', height: '100vh', border: 'none' }}
+        style={{ 
+          width: '100%', 
+          height: '100vh', 
+          border: 'none',
+          overflow: 'hidden',
+        }}
       />
     </div>
   );
